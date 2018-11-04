b24478f9afb6aa391b10c018a47d61d592c09ee3
Jorge Padilla <jorgeepc@Jorges-MacBook-Pro.local>
2018-11-04 13:20:32 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# dotfiles
+
+These are my config settings for various apps, editors, etc. I'd suggest not copying them directly but referencing what I have and what ones might make sense for you.
+
+Enjoy! 😘
---
+++ vscode.js
@@ -0,0 +1,13 @@
+{
+    "bracketPairColorizer.forceIterationColorCycle": true,
+    "editor.cursorWidth": 3,
+    "editor.fontFamily": "'Dank Mono', Menlo, Monaco, 'Courier New', monospace",
+    "editor.fontLigatures": true,
+    "editor.fontSize": 22,
+    "editor.letterSpacing": 0.5,
+    "editor.lineHeight": 40,
+    "editor.tabSize": 2,
+    "files.trimTrailingWhitespace": true,
+    "window.zoomLevel": -1,
+    "workbench.colorTheme": "Night Owl"
+}
