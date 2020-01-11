7ee20c2c13a494309dc79859427cc3b7c1afc004
Jorge Padilla <jorge.esteban.padilla@gmail.com>
2020-01-11 17:51:33 -0500

    Update vscode settings

--- vscode.js
+++ vscode.js
@@ -3,11 +3,14 @@
     "editor.cursorWidth": 3,
     "editor.fontFamily": "'Dank Mono', Menlo, Monaco, 'Courier New', monospace",
     "editor.fontLigatures": true,
-    "editor.fontSize": 22,
-    "editor.letterSpacing": 0.5,
+    "editor.fontSize": 16,
+    "editor.letterSpacing": 0.3,
     "editor.lineHeight": 40,
     "editor.tabSize": 2,
+    "editor.formatOnSave": true,
     "files.trimTrailingWhitespace": true,
-    "window.zoomLevel": -1,
-    "workbench.colorTheme": "Night Owl"
+    "window.zoomLevel": 0,
+    "workbench.colorTheme": "Night Owl",
+    "terminal.integrated.fontFamily": "Inconsolata for Powerline",
+    "terminal.integrated.fontSize": 14
 }
